9519251d48edd5b533c782d13ee1e3f3f155d67f
Edwige08 <edwige.saves@gmail.com>
2025-04-01 09:56:25 +0200

    fin de l'exercice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const filepath = 'data.json';
 
-const content = fs.readFileSync(filepath, {encoding: 'utf8', flag: 'r'});
+const content = fs.readFileSync(filepath, { encoding: 'utf8', flag: 'r' });
 const data = JSON.parse(content);
 // write your code bellow
 
@@ -11,3 +11,22 @@ const data = JSON.parse(content);
 //    3. le nbre d'objets contenus dans le tableau
 //    4. les informations de la candidate arrivée au 4e rang
 //    5. l'âge moyen des candidates 
+
+console.log(typeof data);
+
+console.log(data[0]);
+
+console.log(data.length);
+
+for (let x = 0; x < data.length; x++) {
+    if (data[x].ranking === 4) {
+        console.log(data[x]);
+    }
+}
+
+let ageSum = 0;
+for (let x = 0; x < data.length; x++) {
+    ageSum += data[x].age;
+}
+let ageMoy = ageSum / data.length;
+console.log(ageMoy);
